df2d78ba67d60dba79563f9fae1b899165604e23
Ari Falkner <ari@tryhaventax.com>
2024-11-06 15:08:45 -0500

    empty

--- drizzle/migrations/0000_misty_galactus.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `books` (
-	`book_id` integer PRIMARY KEY NOT NULL,
-	`title` text NOT NULL,
-	`author` text NOT NULL,
-	`publication_date` text NOT NULL,
-	`language` text NOT NULL,
-	`cover_image_url` text,
-	`content` text NOT NULL
-);
